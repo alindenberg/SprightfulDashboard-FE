ef4a86be683f36cd0c0e7b4fd27244f272f094b9
Austin Lindenberg <alindenberg8@gmail.com>
2019-08-30 16:13:06 -0400

    Styling for month and day selector

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,25 +1,38 @@
 <template>
   <b-row class="dashboard">
     <b-col md="6">
-      <b-row class="d-flex justify-content-between">
+      <!-- Header bar -->
+      <b-row class="d-flex justify-content-between" style="margin-left: 2%; margin-right: 2%">
         <h2>Energy Performance</h2>
-        <b-row class="d-flex">
-          <b-dropdown right variant="secondary" :text="timeRangeSelection">
+        <b-row class="d-flex justify-content-between">
+          <b-dropdown right class="btn-sm" variant="info" :text="timeRangeSelection">
             <b-dropdown-item @click="timeRangeSelected('Day')">Day</b-dropdown-item>
             <b-dropdown-item @click="timeRangeSelected('Month')">Month</b-dropdown-item>
             <b-dropdown-item @click="timeRangeSelected('Custom')">Custom</b-dropdown-item>
           </b-dropdown>
-          <datepicker
-            input-class="datepicker"
-            :format="'MM/dd/yyyy'"
-            :value="date"
-            placeholder="Date"
-            name="end_date_picker"
-          ></datepicker>
+          <b-dropdown
+            right
+            class="btn-sm"
+            variant="info"
+            :text="monthSelection"
+            v-if="timeRangeSelection=='Month'"
+          >
+            <b-dropdown-item v-for="(month, index) in months" :key="index">{{month}}</b-dropdown-item>
+          </b-dropdown>
+          <div
+            style="text-align: center"
+            class="d-flex align-items-center"
+            v-if="timeRangeSelection=='Day'"
+          >
+            <button type="button" class="btn btn-link fa fa-angle-left" />
+            <h5
+              style="margin-top: 10px; padding-left: 4px; padding-right: 4px"
+            >{{energyData[selectedIndex]}}</h5>
+            <button type="button" class="btn btn-link fa fa-angle-right" />
+          </div>
         </b-row>
       </b-row>
-      <!-- <day-performance v-if="timeRangeSelection=='Day'" /> -->
-      <!-- <month-performance v-if="timeRangeSelection=='Month'" /> -->
+      <!-- Below header bar w/ date selection -->
       <performance-chart :timestamp="energyData[selectedIndex]" v-if="timeRangeSelection=='Day'" />
       <b-row v-if="timeRangeSelection=='Month'">
         <b-col sm="3" v-for="(data, index) in energyData" :key="index">
@@ -46,12 +59,13 @@ import FplData from "../components/FplData";
 import PerformanceChart from "../components/charts/PerformanceChart";
 import ConsumptionChart from "../components/charts/ConsumptionChart";
 import GenerationChart from "../components/charts/GenerationChart";
-
+import { mdbIcon } from "mdbvue";
 const moment = require("moment-timezone");
 
 export default {
   name: "Dashboard",
   components: {
+    mdbIcon,
     datepicker,
     "neurio-data": NeurioData,
     "fpl-data": FplData,
@@ -75,6 +89,10 @@ export default {
         "2019-01-07"
       ],
       selectedIndex: 1,
+      months: ["August", "Sepetember", "October", "Novembor", "December"],
+      monthSelection: moment()
+        .tz("America/New_York")
+        .format("MMMM"),
       timeRangeSelection: "Day",
       date: moment()
         .tz("America/New_York")
@@ -82,6 +100,14 @@ export default {
     };
   },
   created() {
+    // On load: fetch data for current month
+    // month_data = [
+    //   {
+    //     date: "2019-01-01",
+
+    //   }
+    // ]
+
     // axios
     //   .get(
     //     `${process.env.VUE_APP_API_URL}/locations/f8128cc4-6c70-4458-9f1f-b1d185f3014e/fpl_info`,
@@ -159,7 +185,13 @@ export default {
     clicked(index) {
       console.log("Performance chart clicked with index: ", index);
       this.selectedIndex = index;
+      this.date = moment(this.energyData[this.selectedIndex]).tz(
+        "America/New_York"
+      );
       this.timeRangeSelection = "Day";
+    },
+    iconClicked(value) {
+      console.log(`${value} icon clicked`);
     }
   }
 };
